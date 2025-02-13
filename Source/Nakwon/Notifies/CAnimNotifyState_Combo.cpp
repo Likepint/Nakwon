bea752b60268c0bfc05a5f15da29f62ceae61646
jeongsupark <jsuuu96@naver.com>
2025-02-13 19:52:08 +0900

    Notify 제작

---
+++ Source/Nakwon/Notifies/CAnimNotifyState_Combo.cpp
@@ -0,0 +1,44 @@
+#include "Notifies/CAnimNotifyState_Combo.h"
+#include "Global.h"
+#include "Components/CWeaponComponent.h"
+#include "Weapons/CDoAction.h"
+#include "Weapons/Combo/CDoAction_Combo.h"
+
+FString UCAnimNotifyState_Combo::GetNotifyName_Implementation() const
+{
+	return "Combo";
+}
+
+void UCAnimNotifyState_Combo::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration);
+
+	NullCheck(MeshComp);
+	NullCheck(MeshComp->GetOwner());
+
+	UCWeaponComponent* weapon = CHelpers::GetComponent<UCWeaponComponent>(MeshComp->GetOwner());
+	NullCheck(weapon);
+	NullCheck(weapon->GetDoAction());
+
+	UCDoAction_Combo* combo = Cast<UCDoAction_Combo>(weapon->GetDoAction());
+	NullCheck(combo);
+
+	combo->EnableCombo();
+}
+
+void UCAnimNotifyState_Combo::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::NotifyEnd(MeshComp, Animation);
+
+	NullCheck(MeshComp);
+	NullCheck(MeshComp->GetOwner());
+
+	UCWeaponComponent* weapon = CHelpers::GetComponent<UCWeaponComponent>(MeshComp->GetOwner());
+	NullCheck(weapon);
+	NullCheck(weapon->GetDoAction());
+
+	UCDoAction_Combo* combo = Cast<UCDoAction_Combo>(weapon->GetDoAction());
+	NullCheck(combo);
+
+	combo->DisableCombo();
+}
